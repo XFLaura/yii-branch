ec85532bf1c3b2052849a0552069867ecb68e7a5
郗菲 <xifei1@staff.sina.com.cn>
2019-03-30 10:01:55 +0800

    first commit

---
+++ models/EntryForm.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * User: xifei
+ * Date: 2019/3/29
+ * Time: ÏÂÎç6:00
+ */
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class EntryForm extends Model
+{
+    public $name;
+    public $email;
+
+    public function rules()
+    {
+        return [
+            [['name', 'email'], 'required'],
+            ['email', 'email'],
+        ];
+    }
+}
---
+++ views/site/say.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * User: xifei
+ * Date: 2019/3/29
+ * Time: ÏÂÎç5:55
+ */
+
+use yii\helpers\Html;
+?>
+<?= Html::encode($message) ?>
