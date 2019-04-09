e25563e93191e15cf2bddc1260308e3e8707ed36
郗菲 <xifei1@staff.sina.com.cn>
2019-04-09 18:50:30 +0800

    request

---
+++ controllers/CountryController_request.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\Country;
+use app\models\CountrySearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\components\Respone;
+
+/**
+ * CountryController implements the CRUD actions for Country model.
+ */
+class CountryController extends Controller
+{
+
+
+    /**
+     * 重写缺省动作
+     */
+    public function __construct($id, $module, $config = [])
+    {
+        parent::__construct($id, $module, $config = []);
+        $this->defaultAction = 'create';
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Country models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        /*
+         * request 组件提供了许多方式来检测当前请求的 URL。
+         */
+        echo \Yii::$app->request->absoluteUrl;
+        echo \Yii::$app->request->Url;
+
+        echo  Yii::$app->request->userIP;
+
+        /**
+         * 服务器异常状态抛出
+         */
+        throw new \yii\web\UnsupportedMediaTypeHttpException;
+
+        /**
+         * 响应主体
+         */
+        echo  Yii::$app->response->content = 'hello world!';
+
+
+        $searchModel = new CountrySearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Country model.
+     * @param string $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Country model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Country();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->code]);
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Updates an existing Country model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param string $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->code]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Deletes an existing Country model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param string $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Country model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param string $id
+     * @return Country the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Country::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
